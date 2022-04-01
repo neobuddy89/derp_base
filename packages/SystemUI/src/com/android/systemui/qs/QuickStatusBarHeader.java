6beaf6bea1d293934a21a395b743cf25b6784484
IacobIonut01 <paulionut2003@gmail.com>
2022-04-01 17:38:27 +0000

    Quick Settings: Redesign for DotOS 6.0
    - Brings up the A11 style of tiles
      + Added morph status of tiles (Circle - Active | Rounded Rect - Inactive/Disabled)
      + Kept compatibility with the secondaryLabel from A12
    - Initial support for columns/rows control in QQS, QS and Customizer
    - Removed color differencce between QSPanel and Notification Drawer
    - Aligned Notification Footer buttons with the notifications (History | Clear All)
    - Added transparency for blur support
    [Temporary removed] - Keep showing notifications when the QSPanel is fully expanded
    
    Bugs / W.I.P.....
    [Temporary removed] - After exiting the QS Customizer, the Notification Drawer's scrim color goes to TRANSPARENT until the user scroll slightly
    
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Change-Id: I9630d414cd6db3b896dcc6f0591b1cd728012f36

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -79,7 +79,6 @@ public class QuickStatusBarHeader extends FrameLayout implements
     private View mStatusIconsView;
     private View mContainer;
 
-    private View mQSCarriers;
     private ViewGroup mClockContainer;
     private Clock mClockView;
     private Space mDatePrivacySeparator;
@@ -104,10 +103,6 @@ public class QuickStatusBarHeader extends FrameLayout implements
     private float mKeyguardExpansionFraction;
     private int mTextColorPrimary = Color.TRANSPARENT;
     private int mTopViewMeasureHeight;
-
-    @NonNull
-    private List<String> mRssiIgnoredSlots;
-    private boolean mIsSingleCarrier;
 
     private boolean mHasCenterCutout;
     private boolean mConfigShowBatteryEstimate;
@@ -138,7 +133,6 @@ public class QuickStatusBarHeader extends FrameLayout implements
         mHeaderQsPanel = findViewById(R.id.quick_qs_panel);
         mDatePrivacyView = findViewById(R.id.quick_status_bar_date_privacy);
         mStatusIconsView = findViewById(R.id.quick_qs_status_icons);
-        mQSCarriers = findViewById(R.id.carrier_group);
         mContainer = findViewById(R.id.qs_container);
         mIconContainer = findViewById(R.id.statusIcons);
         mPrivacyChip = findViewById(R.id.privacy_chip);
@@ -186,7 +180,6 @@ public class QuickStatusBarHeader extends FrameLayout implements
             StatusBarContentInsetsProvider insetsProvider) {
         mUseCombinedQSHeader = useCombinedQSHeader;
         mTintedIconManager = iconManager;
-        mRssiIgnoredSlots = rssiIgnoredSlots;
         mInsetsProvider = insetsProvider;
         int fillColor = Utils.getColorAttrDefaultColor(getContext(),
                 android.R.attr.textColorPrimary);
@@ -196,11 +189,6 @@ public class QuickStatusBarHeader extends FrameLayout implements
 
         mQSExpansionPathInterpolator = qsExpansionPathInterpolator;
         updateAnimators();
-    }
-
-    void setIsSingleCarrier(boolean isSingleCarrier) {
-        mIsSingleCarrier = isSingleCarrier;
-        updateAlphaAnimator();
     }
 
     public QuickQSPanel getHeaderQsPanel() {
@@ -386,15 +374,10 @@ public class QuickStatusBarHeader extends FrameLayout implements
 //                .addFloat(mDateView, "alpha", 0, 0, 1)
 //                .addFloat(mClockDateView, "alpha", 1, 0, 0)
                 .addFloat(mClockView, "alpha", 0, 1)
-                .addFloat(mQSCarriers, "alpha", 0, 1)
                 .setListener(new TouchAnimator.ListenerAdapter() {
                     @Override
                     public void onAnimationAtEnd() {
                         super.onAnimationAtEnd();
-                        if (!mIsSingleCarrier) {
-                            mIconContainer.addIgnoredSlots(mRssiIgnoredSlots);
-                        }
-                        // Make it gone so there's enough room for carrier names
                         mClockDateView.setVisibility(View.GONE);
                     }
 
@@ -403,9 +386,6 @@ public class QuickStatusBarHeader extends FrameLayout implements
 //                        mClockDateView.setVisibility(View.VISIBLE);
 //                        mClockDateView.setFreezeSwitching(true);
                         setSeparatorVisibility(false);
-                        if (!mIsSingleCarrier) {
-                            mIconContainer.addIgnoredSlots(mRssiIgnoredSlots);
-                        }
                     }
 
                     @Override
@@ -414,8 +394,6 @@ public class QuickStatusBarHeader extends FrameLayout implements
 //                        mClockDateView.setFreezeSwitching(false);
 //                        mClockDateView.setVisibility(View.VISIBLE);
                         setSeparatorVisibility(mShowClockIconsSeparator);
-                        // In QQS we never ignore RSSI.
-                        mIconContainer.removeIgnoredSlots(mRssiIgnoredSlots);
                     }
                 });
         mAlphaAnimator = builder.build();
@@ -546,7 +524,6 @@ public class QuickStatusBarHeader extends FrameLayout implements
         if (mClockIconsSeparator.getVisibility() == newVisibility) return;
 
         mClockIconsSeparator.setVisibility(visible ? View.VISIBLE : View.GONE);
-        mQSCarriers.setVisibility(visible ? View.GONE : View.VISIBLE);
 
         LinearLayout.LayoutParams lp =
                 (LinearLayout.LayoutParams) mClockContainer.getLayoutParams();
