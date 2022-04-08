AospExtended/platform_frameworks_base

Use alphaoptimized layout for custom icons on left
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
Change-Id: I94e7f725c658be5182fc4ce6990906c5e94c776f

packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -23,6 +23,7 @@ import android.graphics.Rect;
 import android.view.DisplayCutout;
 import android.view.View;
 import android.view.WindowInsets;
+import android.widget.LinearLayout;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.widget.ViewClippingUtil;
@@ -61,6 +62,7 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
     private final View mClockView;
     private final View mOperatorNameView;
     private final View mAEXLogoView;
+    private final LinearLayout mCustomIconArea;
     private final DarkIconDispatcher mDarkIconDispatcher;
     private final NotificationPanelView mPanelView;
     private final Consumer<ExpandableNotificationRow>
@@ -109,7 +111,8 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
                 statusbarView.findViewById(R.id.clock),
                 statusbarView.findViewById(R.id.operator_name_frame),
                 statusbarView.findViewById(R.id.centered_icon_area),
-                statusbarView.findViewById(R.id.status_bar_logo));
+                statusbarView.findViewById(R.id.status_bar_logo),
+                statusbarView.findViewById(R.id.left_icon_area));
     }
 
     @VisibleForTesting
@@ -125,7 +128,8 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
             View clockView,
             View operatorNameView,
             View centeredIconView,
-            View aexLogoView) {
+            View aexLogoView,
+            LinearLayout customIconArea) {
         mNotificationIconAreaController = notificationIconAreaController;
         mHeadsUpManager = headsUpManager;
         mHeadsUpManager.addListener(this);
@@ -144,6 +148,7 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
         mClockView = clockView;
         mAEXLogoView = aexLogoView;
         mOperatorNameView = operatorNameView;
+        mCustomIconArea = customIconArea;
         mDarkIconDispatcher = Dependency.get(DarkIconDispatcher.class);
         mDarkIconDispatcher.addDarkReceiver(this);
 
@@ -294,6 +299,7 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
                if (mAEXLogoView.getVisibility() != View.GONE) {
                     hide(mAEXLogoView, View.INVISIBLE);
                 }
+                hide(mCustomIconArea, View.INVISIBLE);
             } else {
                 if (((Clock)mClockView).shouldBeVisible()) {
                     show(mClockView);
@@ -307,6 +313,7 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
                if (mAEXLogoView.getVisibility() != View.GONE) {
                     show(mAEXLogoView);
                 }
+                show(mCustomIconArea);
                 hide(mHeadsUpStatusBarView, View.GONE, () -> {
                     updateParentClipping(true /* shouldClip */);
                 });

neobuddy89/derp_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -23,6 +23,7 @@ import android.graphics.Rect;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.widget.ViewClippingUtil;
@@ -67,6 +68,7 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
     private final View mClockView;
     private final View mOperatorNameView;
     private final View mDerpLogoView;
+    private final LinearLayout mCustomIconArea;
     private final DarkIconDispatcher mDarkIconDispatcher;
     private final NotificationPanelViewController mNotificationPanelViewController;
     private final Consumer<ExpandableNotificationRow>
@@ -115,7 +117,8 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
                 statusBarView.findViewById(R.id.clock),
                 statusBarView.findViewById(R.id.operator_name_frame),
                 statusBarView.findViewById(R.id.centered_icon_area),
-                statusBarView.findViewById(R.id.status_bar_logo));
+                statusBarView.findViewById(R.id.status_bar_logo),
+                statusBarView.findViewById(R.id.left_icon_area));
     }
 
     @VisibleForTesting
@@ -133,7 +136,8 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
             View clockView,
             View operatorNameView,
             View centeredIconView,
-            View derpLogoView) {
+            View derpLogoView,
+            LinearLayout customIconArea) {
         super(headsUpStatusBarView);
         mNotificationIconAreaController = notificationIconAreaController;
         mHeadsUpManager = headsUpManager;
@@ -153,6 +157,7 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
         mClockView = clockView;
         mDerpLogoView = derpLogoView;
         mOperatorNameView = operatorNameView;
+        mCustomIconArea = customIconArea;
         mDarkIconDispatcher = Dependency.get(DarkIconDispatcher.class);
 
         mView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
@@ -257,6 +262,7 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
                if (mDerpLogoView.getVisibility() != View.GONE) {
                     hide(mDerpLogoView, View.INVISIBLE);
                 }
+                hide(mCustomIconArea, View.INVISIBLE);
             } else {
                 if (clockStyle == 0 && isClockVisible) {
                     show(mClockView);
@@ -272,6 +278,7 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
                 if (mDerpLogoView.getVisibility() != View.GONE) {
                     show(mDerpLogoView);
                 }
+                show(mCustomIconArea);
                 hide(mView, View.GONE, () -> {
                     updateParentClipping(true /* shouldClip */);
                 });