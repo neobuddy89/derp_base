PixelExperience/frameworks_base

SystemUI: Introduce ColumbusCompatibilityHelper
Change-Id: Idc61087f629cd929b67a1f517a32055be3210210

packages/SystemUI/src/org/pixelexperience/systemui/columbus/ColumbusCompatibilityHelper.java
@@ -1,0 +1,17 @@
+package org.pixelexperience.systemui.columbus;
+
+import android.os.SystemProperties;
+
+public class ColumbusCompatibilityHelper {
+    public static boolean useApSensor() {
+        return SystemProperties.getBoolean("persist.columbus.use_ap_sensor", true);
+    }
+
+    public static String getModelFileName() {
+        return SystemProperties.get("persist.columbus.model", "tap7cls_crosshatch.tflite");
+    }
+
+    public static Long apSensorThrottleMs() {
+        return SystemProperties.getLong("persist.columbus.ap_sensor.throttle_ms", 500);
+    }
+}

neobuddy89/derp_base

packages/SystemUI/src/org/derpfest/systemui/columbus/ColumbusCompatibilityHelper.java
@@ -1,0 +1,17 @@
+package org.derpfest.systemui.columbus;
+
+import android.os.SystemProperties;
+
+public class ColumbusCompatibilityHelper {
+    public static boolean useApSensor() {
+        return SystemProperties.getBoolean("persist.columbus.use_ap_sensor", true);
+    }
+
+    public static String getModelFileName() {
+        return SystemProperties.get("persist.columbus.model", "tap7cls_crosshatch.tflite");
+    }
+
+    public static Long apSensorThrottleMs() {
+        return SystemProperties.getLong("persist.columbus.ap_sensor.throttle_ms", 500);
+    }
+}