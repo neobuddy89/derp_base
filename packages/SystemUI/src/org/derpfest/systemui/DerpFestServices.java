30151e6adeccdffaedc4de48a193751cfce08dfd
spezi77 <spezi7713@gmx.net>
2022-05-07 11:27:19 +0200

    DerpFestServices: Disable google elmyra
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Ifcc09139af494289cda0ea059b2d360b32bd1155

--- packages/SystemUI/src/org/derpfest/systemui/DerpFestServices.java
+++ packages/SystemUI/src/org/derpfest/systemui/DerpFestServices.java
@@ -61,10 +61,10 @@ public class DerpFestServices extends VendorServices {
 
     @Override
     public void start() {
-        if (mContext.getPackageManager().hasSystemFeature("android.hardware.context_hub")
+        /*if (mContext.getPackageManager().hasSystemFeature("android.hardware.context_hub")
                 && new ElmyraContext(mContext).isAvailable()) {
             addService(new ElmyraService(mContext, mServiceConfigurationGoogle.get(), mUiEventLogger));
-        }
+        }*/
         if (new ColumbusContext(mContext).isAvailable()) {
             addService(mColumbusServiceLazy.get());
         }
